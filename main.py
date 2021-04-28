64ae8a0ae3d1b4fd400219374d0ae190301a68cb
Jacques Saraffe <jsarraffe@gmail.com>
2021-04-28 14:02:49 -0800

    yessir

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import matplotlib.pyplot as plt
+# plt.plot([1,2,3,4])
+# plt.ylabel('some numbers')
+# plt.show()
+
+
+
 def readFile(filename, array):
     file = open("./KnapsackTestData/" + filename, "r")
     temp = []
@@ -6,6 +13,8 @@ def readFile(filename, array):
     for x in temp:
         array.append(int(x.strip().replace("\n", '')))
 
+
+# ------------------------------------GLOBAL VARIABLES----------------------------------
 capacity = []
 values = [0]
 weights = [0]
@@ -19,32 +28,29 @@ readFile('p00_w.txt', weights)
 # print(capacity)
 oneAtable = [[0 for j in range(capacity[0] + 1)] for i in range(1, len(values) + 1)]
 oneBTable = [[0 for j in range(capacity[0] + 1)] for i in range(1, len(values) + 1)]
+# ---------------------------------------------------------------------------------------
 for i in range(1, len(oneBTable)):
     for j in range(1, len(oneAtable[0])):
         oneBTable[i][j] = -1
 
 
-
-
-
-
-
-def knapsack(values, weights, ksTable):
+def knapsack(values, weights):
     # fill table
     for i in range(1, len(values)):
-        for j in range(1, len(ksTable[i])):
+        for j in range(1, len(oneAtable[i])):
             if j - weights[i] < 0:
-                ksTable[i][j] = ksTable[i - 1][j]
+                oneAtable[i][j] = oneAtable[i - 1][j]
             else:
-                ksTable[i][j] = max(ksTable[i - 1][j], values[i] + ksTable[i - 1][j - weights[i]])
-    for i in ksTable:
-        print(i)
+                oneAtable[i][j] = max(oneAtable[i - 1][j], values[i] + oneAtable[i - 1][j - weights[i]])
+
+    # for i in ksTable:
+    #     print(i)
     # back trace
     i = len(values) - 1
-    j = len(ksTable[0]) - 1
+    j = len(oneAtable[0]) - 1
     answer = []
     while i != 0:
-        if ksTable[i - 1][j] < ksTable[i][j]:
+        if oneAtable[i - 1][j] < oneAtable[i][j]:
             answer.append(i)
             j = j - weights[i]
             i -= 1
@@ -53,8 +59,60 @@ def knapsack(values, weights, ksTable):
     result = answer[::-1]
     return result
 
-def MFKnapsack(i,j):
-    return 1
 
-print(knapsack(values, weights, oneAtable))
+def MFKnapsack(i, j):
+    if oneBTable[i][j] < 0:
+        if j < weights[i]:
+            value = MFKnapsack(i - 1, j)
+        else:
+            value = max(MFKnapsack(i - 1, j), values[i] + MFKnapsack(i - 1, j - weights[i]))
+        oneBTable[i][j] = value
+    return oneBTable[i][j]
 
+
+# ------------------------------------GLOBAL VARIABLES----------------------------------
+value = 0
+
+
+# --------------------------------------------------------------------------------------
+
+def oneBKnapSack():
+    i = len(values) - 1
+    j = capacity[0]
+
+    # memory function
+    MFKnapsack(i, j)
+
+
+    #
+    # for i in oneBTable:
+    #     print(i)
+
+
+
+    # back trace
+    i = len(values) - 1
+    j = len(oneBTable[0]) - 1
+    answer = []
+    while i != 0:
+        if oneBTable[i - 1][j] < oneBTable[i][j]:
+            answer.append(i)
+            j = j - weights[i]
+            i -= 1
+        else:
+            i -= 1
+    result = answer[::-1]
+    return result
+
+
+# print(knapsack(values, weights, oneAtable))
+
+
+oneA = knapsack(values, weights)
+oneB = oneBKnapSack()
+
+print("Solution to 1A")
+print(oneA)
+
+print("Solution to 1B")
+print(oneB)
